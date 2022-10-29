f4a5bbc35df29d13df10ba3dbdaaae286ead307e
HugoDemont <91455631+HugoDemont62@users.noreply.github.com>
2022-10-29 14:02:02 +0200

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -25,39 +25,3 @@ if($mail!=NULL){
 //redirect
 header("Location:index.html");
 ?>
-
-
-
-<?php
-function IsInjected($str)
-{
-    $injections = array('(\n+)',
-           '(\r+)',
-           '(\t+)',
-           '(%0A+)',
-           '(%0D+)',
-           '(%08+)',
-           '(%09+)'
-           );
-               
-    $inject = join('|', $injections);
-    $inject = "/$inject/i";
-    
-    if(preg_match($inject,$str))
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-}
-
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
-
-?>
-
